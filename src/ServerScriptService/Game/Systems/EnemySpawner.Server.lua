e81645f2e9e043d4ff5f6de9820b69d08883ae73
KancerProductions <productionskancer@gmail.com>
2025-09-19 14:09:26 -0500

    Delete src/ServerScriptService/Game/Systems/EnemySpawner.Server.lua

--- src/ServerScriptService/Game/Systems/EnemySpawner.Server.lua
+++
@@ -1,8 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Config = require(ReplicatedStorage.Modules.Shared["Config.Module"])
-local currentWave = 0
-function _G.StartWave()
-    currentWave += 1
-    local count = math.floor(Config.WAVES.START_COUNT * (Config.WAVES.GROWTH^(currentWave-1)))
-    print("[Spawner] Wave", currentWave, "spawn", count)
-end

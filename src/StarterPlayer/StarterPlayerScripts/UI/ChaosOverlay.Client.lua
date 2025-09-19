e205736cafc6339d950678423ff19bb376b9d092
KancerProductions <productionskancer@gmail.com>
2025-09-19 14:10:20 -0500

    Delete src/StarterPlayer/StarterPlayerScripts/UI/ChaosOverlay.Client.lua

--- src/StarterPlayer/StarterPlayerScripts/UI/ChaosOverlay.Client.lua
+++
@@ -1,3 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Net = require(ReplicatedStorage.Net.Events)
-Net.Get("ChaosBroadcast").OnClientEvent:Connect(function(p) if typeof(p)=="table" and p.name then print("[UI] CHAOS:", p.name, "("..p.duration.."s)") end end)

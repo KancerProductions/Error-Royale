98d6077d4464aff348e215ebcac72bffd12fb61a
KancerProductions <productionskancer@gmail.com>
2025-09-19 14:09:04 -0500

    Delete src/ServerScriptService/Game/Services/ShopService.Server.lua

--- src/ServerScriptService/Game/Services/ShopService.Server.lua
+++
@@ -1,3 +0,0 @@
-local ServerStorage = game:GetService("ServerStorage")
-local Cosmetics = require(ServerStorage.Data:WaitForChild("Cosmetics"))
-print("[ShopService] Loaded", #Cosmetics.AURAS, "auras,", #Cosmetics.LOOT_BEAMS, "beams")
